f5bfd9ae41c0e6bb0a9e70e366f9958eb64a7d81
muriithiderro <muriithiderrick56@gmail.com>
2018-04-24 23:34:55 +0300

    initialiazed project files

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,25 +1,23 @@
 
-import { Component, OnInit, Input} from '@angular/core';
-import {ServeDataService} from '../serve-data.service'
+import { Component, OnInit} from '@angular/core';
 import { Quote } from '../quote-model'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css'],
-   providers : [ServeDataService]
+
 })
 export class QuoteComponent implements OnInit {
-  @Input() quotes
+
+  quotes :Quote[]
+
+  receivedObj(e){
+    this.quotes = e;  
+  }
+   
   	sortedQuote(): Quote[] {
      return this.quotes.sort((a, b) => b.upvotes - a.upvotes)
    }
-  constructor(private senddata:ServeDataService) { 
-    this.quotes = []
-    this.senddata
-  }
-  ngOnInit() {
-  }
-
   voteUp(id:number){
   	this.quotes[id].upVote()
 
@@ -29,20 +27,14 @@ export class QuoteComponent implements OnInit {
   	
   }
   delete(id:number){
-    this.senddata.logData("deleting the quote titles:"+ this.quotes[id].quote)
-  	this.quotes.splice(id)
+  	this.quotes.splice(id,1)
    }
-  getColor(id:number){
-  	if(this.quotes[id].upvotes > this.quotes[id].downvotes){
-  		return 'green'
-  	}else if(this.quotes[id].downvotes >this.quotes[id].upvotes){
-  		return 'red'
-  	}else{
-  		return 'magenta'
-  	}
- }
- seeThis(id){
- 	return Math.max(...this.quotes[id].upvotes)
- }
+
+  constructor() { 
+    this.quotes = []
+   
+  }
+  ngOnInit() {
+  }
 }
 
